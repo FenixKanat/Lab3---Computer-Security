d4eaba9c46edfe40a715b67148c487a58f47fe1c
FenixKanat <fenix.k.97@outlook.com>
2025-10-10 12:50:45 +0200

    finished register route

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 DATABASE = 'users.db'
 
 def init_db():
-    with sqlite3.connect(DATABASE) as connect:
-        connect.execute('''CREATE TABLE IF NOT EXISTS users(
+    with sqlite3.connect(DATABASE) as conn:
+        conn.execute('''CREATE TABLE IF NOT EXISTS users(
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         username TEXT UNIQUE NOT NULL,
                         password_hash TEXT NOT NULL,
@@ -15,6 +15,23 @@ def init_db():
 
 @app.route('/register', methods = ['POST'])
 def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get ('password')
+
+    if not username or not password:
+        return jsonify({'error' : 'Username and password required'}),400
+    
+    password_hash = generate_password_hash(password)
+
+    try:
+
+        with sqlite3.connect(DATABASE) as conn:
+            conn.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)",
+                         (username, password_hash))
+            return jsonify({'message' : 'User registered successfully'}),201
+    except sqlite3.IntegrityError:
+        return jsonify({'error': 'Username already exists'}),409
 
 
 
