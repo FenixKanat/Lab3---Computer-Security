6dd96c8da51c9646f0e7d667db06ad426c9680d7
FenixKanat <fenix.k.97@outlook.com>
2025-10-10 12:33:40 +0200

    initial setup

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
+from flask import Flask, request, jsonify
+from werkzeug.security import generate_password_hash,check_password_hash
+import sqlite3
 
-print ("hej på dig")
+app = Flask(__name__)
+
+@app.route('/register', methods = ['POST'])
+@app.route('/login', methods = ['POST'])
+@app.route('/mfa/setup', methods = ['POST'])
+@app.route('/mfa/verify', methods = ['POST'])
+
