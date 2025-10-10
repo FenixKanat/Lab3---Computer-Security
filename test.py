3734b2fafd5faf83eebef6027b2af1c6851ea46f
FenixKanat <fenix.k.97@outlook.com>
2025-10-10 12:41:35 +0200

    starting with the database and routes

--- test.py
+++ test.py
@@ -3,9 +3,25 @@ from werkzeug.security import generate_password_hash,check_password_hash
 import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'users.db'
+
+def init_db():
+    with sqlite3.connect(DATABASE) as connect:
+        connect.execute('''CREATE TABLE IF NOT EXISTS users(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT UNIQUE NOT NULL,
+                        password_hash TEXT NOT NULL,
+                        mfa_secret TEXT)''')
 
 @app.route('/register', methods = ['POST'])
+def register():
+
+
+
 @app.route('/login', methods = ['POST'])
+def login():
+
+
 @app.route('/mfa/setup', methods = ['POST'])
 @app.route('/mfa/verify', methods = ['POST'])
 
